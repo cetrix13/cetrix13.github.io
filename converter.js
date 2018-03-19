190f216935af784de5f31e95cbf8be9b5b9f246b
Alexey Tomin <alextomin@list.ru>
2018-03-19 23:37:05 +0100

    Fix port problem, use original movie titles

--- converter.js
+++ converter.js
@@ -5,11 +5,12 @@ const readFile = p(fs.readFile)
 const writeFile = p(fs.writeFile)
 
 async function main() {
-  const moviesJson = await readFile('./moviesList.json')
+  const moviesJson = await readFile('./moviesListOrigin.json')
   const movies = JSON.parse(moviesJson).movies
-  const moviesRes = movies.map((m, index) => ({
+  const moviesRes = movies.map(({title, originalTitle, ...m}, index) => ({
     ...m,
-    id: index + 1
+    id: index + 1,
+    title: originalTitle || title
   }))
   const res = { movies: moviesRes }
   await fs.writeFile('./movies.json', JSON.stringify(res, null, 2))
