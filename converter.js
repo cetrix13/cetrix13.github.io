1a7c314ca1e25b9e6db4885a0749c45df20b74e3
Alexander Dadykin <alexander_dadykin@epam.com>
2023-10-19 21:57:24 +0200

    Update movies list with correct images

--- converter.js
+++ converter.js
@@ -4,6 +4,17 @@ const fs = require('fs')
 const readFile = p(fs.readFile)
 const writeFile = p(fs.writeFile)
 
+
+function checkImageExist(url, id) {
+  return fetch(url, { method: 'HEAD'}).then(res => {
+    if (res.ok) {
+      return id
+    } else {
+      return false
+    }
+  }).catch(err => console.log(err))
+}
+
 async function main() {
   const moviesJson = await readFile('./moviesListOrigin.json')
   const movies = JSON.parse(moviesJson).movies
@@ -12,8 +23,14 @@ async function main() {
     id: index + 1,
     title: originalTitle || title
   }))
-  const res = { movies: moviesRes }
-  await fs.writeFile('./movies.json', JSON.stringify(res, null, 2), (err) => err && console.error(err))
+
+  const promises = moviesRes.map((m) => checkImageExist(m.posterurl, m.id));
+  const ids = (await Promise.all(promises)).filter((p) => p)
+
+  const moviesWithValidImages = moviesRes.filter((m) => ids.includes(m.id));
+
+  const res = { movies: moviesWithValidImages }
+  await fs.writeFile('./movies.json', JSON.stringify(res, null, 2), (err) => err && console.error(err))  
 }
 
 main()
